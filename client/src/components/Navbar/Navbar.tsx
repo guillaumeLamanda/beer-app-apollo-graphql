b245f257f1a500ff75a4bf3e516c0c8838eba020
Guillaume Lamanda <guillaume.lamanda@zenika.com>
2020-04-28 00:30:24 +0200

    ♻️ improve <Navbar> behavior on mobile

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar: FC<Props> = ({ userName, onAuthClick }) => (
     align="center"
   >
     <Heading size="small">Beer App</Heading>
-    <Box direction="row" gap="small">
+    <Box direction="row-responsive" gap="small">
       <AnchorLink color="text-strong" to="/" label="fil" />
       <AnchorLink color="text-strong" to="/beers" label="bières" />
       <AnchorLink color="text-strong" to="/users" label="utilisateurs" />
