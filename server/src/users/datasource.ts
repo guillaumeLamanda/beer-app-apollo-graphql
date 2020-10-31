ef86d0d49a63a095b8ac4326785d37971b01ee6c
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2020-10-31 12:50:05 +0100

    ♻️ update datasources to use prisma client

--- server/src/users/datasource.ts
+++ server/src/users/datasource.ts
@@ -1,54 +1,63 @@
 import { User } from "@ba/schema";
 import { DbDatasource } from "../database/datasource";
-import { UserModel } from "../database/user.model";
-import { UserBeersModel } from "../database/beer.model";
 
 export class UserDataSource extends DbDatasource {
   async findById(id: string) {
-    return UserModel.findByPk(id, { include: [{ all: true }] });
+    return this.db.user.findOne({ where: { id }, include: { beers: true } });
   }
 
   async findByName(name: string) {
-    return UserModel.findOne({ where: { name } });
+    return this.db.user.findOne({ where: { name } });
   }
 
   async find() {
-    return UserModel.findAll({ include: [UserBeersModel] });
+    return this.db.user.findMany();
   }
 
   async create({ name }: { name: string }) {
-    return UserModel.create({ name }, { include: [{ all: true }] });
+    return this.db.user.create({ data: { name } });
   }
 
   async update(id: string, data: Partial<User>) {
-    const user = await UserModel.findByPk(id, { include: [{ all: true }] });
-    return user.update({
-      ...data,
-      ...(data.beers && data.beers.map(({ id }) => id)),
+    return this.db.user.update({
+      where: { id },
+      data: {
+        ...data,
+        ...(data.beers && {
+          beers: {
+            set: data.beers.map(({ id }) => ({
+              id,
+            })),
+          },
+        }),
+      },
     });
   }
 
   async toogleBeerLike(userId: string, beerId: string) {
-    const user = await UserModel.findByPk(userId, { include: [{ all: true }] });
-    if (
-      user.beers.some(({ beerId: storedBeerId }) => storedBeerId === beerId)
-    ) {
-      console.log("to destroy");
-      const beer = await user.beers.find(
-        ({ beerId: storedBeerId }) => storedBeerId === beerId
-      );
-      await beer.destroy();
-      user.set(
-        "beers",
-        user.beers.filter(({ beerId: storedBeerId }) => storedBeerId !== beerId)
-      );
-    } else {
-      const beer = await UserBeersModel.create({
-        beerId,
-        userId,
-      });
-      user.set("beers", [...user.beers, beer]);
-    }
-    return user.save();
+    const user = await this.db.user.findOne({
+      where: { id: userId },
+      select: {
+        beers: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+    const beers = user.beers.some(({ id }) => id === beerId)
+      ? user.beers.filter(({ id }) => id !== beerId)
+      : [...user.beers, { id: beerId }];
+
+    this.db.user.update({
+      where: { id: userId },
+      data: {
+        beers: {
+          set: beers,
+        },
+      },
+    });
+
+    return this.findById(userId);
   }
 }
